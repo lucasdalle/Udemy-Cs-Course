f5fa33263e5bd35548f66cff80f928fb943f4d45
Lucas Dalle <lucas.dallemole@gmail.com>
2024-03-14 14:36:43 -0300

    Version 1

---
+++ Exercise15-Delegates/Exercise15-Delegates/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Numerics;
+
+namespace Exercise15_Delegates
+{
+    internal class Program
+    {
+        public delegate float OperationDelegate(float num1, float num2);
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ApplyOperation(1,2, Add));
+            Console.WriteLine(ApplyOperation(1,2, Subtract));
+            Console.WriteLine(ApplyOperation(1,2, Multiply));
+            Console.WriteLine(ApplyOperation(1,2, Divide));
+        }
+
+        public static float ApplyOperation(float num1, float num2, OperationDelegate op)
+        {
+            return op(num1, num2);
+        }
+
+        public static float Add(float num1, float num2)
+        {
+            return num1 + num2;
+        }
+
+        public static float Subtract(float num1, float num2)
+        {
+            return num1 - num2;
+        }
+
+        public static float Multiply(float num1, float num2)
+        {
+            return num1 * num2;
+        }
+
+        public static float Divide(float num1, float num2)
+        {
+            return num1 / num2;
+        }
+    }
+}
