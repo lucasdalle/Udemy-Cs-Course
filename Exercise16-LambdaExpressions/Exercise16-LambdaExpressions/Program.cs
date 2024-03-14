316fc8b5e9e758e0abd811b2fd12939c8cc7f331
Lucas Dalle <lucas.dallemole@gmail.com>
2024-03-14 15:15:42 -0300

    Version 1

---
+++ Exercise16-LambdaExpressions/Exercise16-LambdaExpressions/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercise16_LambdaExpressions
+{
+    internal class Program
+    {
+        public static Dictionary<string, Func<float, float, float>> Operators = new Dictionary<string, Func<float, float, float>>
+        {
+            { "+", (num1, num2) => num1 + num2 },
+            { "-", (num1, num2) => num1 - num2 },
+            { "/", (num1, num2) => num1 / num2 },
+            { "*", (num1, num2) => num1 * num2 }
+        };
+
+        public static Func<float, float, float> OperationGet(string operation)
+        {
+            if(Operators.ContainsKey(operation))
+            {
+                return Operators[operation];
+            }
+            else 
+                return null;
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
